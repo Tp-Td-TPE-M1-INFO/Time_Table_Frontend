b263d2cc9f4838e71a5baf153c6d105d57697bb6
Jugalux <jugalux111@gmail.com>
2023-04-25 09:58:59 +0100

    some changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from '../src/media/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  )
+}
+
+export default App;
