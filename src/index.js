d8a675ebf13cc2048b80e87550c98825e5bcc70a
jocelyn pyw <jocelynwotcheu2@gmail.com>
2023-05-07 01:05:06 +0100

    ajout de redux

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ProSidebarProvider } from "react-pro-sidebar";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ProSidebarProvider>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </ProSidebarProvider>
     </BrowserRouter>
   </React.StrictMode>
