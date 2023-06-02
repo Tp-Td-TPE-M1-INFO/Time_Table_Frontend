aaacbe85fafe86691a9b56fa7cfcc596b0c6efe7
EssiJunior <nessipjunior@example.com>
2023-06-02 12:27:09 +0100

    feat(app): NOT FOUND PAGE Styled

--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "../styles/notFound.css";
 
+import image from "../assets/images/notfound.gif";
+
 function NotFound() {
   return (
     <div className="container">
       <h1 className="title">OUPS!!!!!!!</h1>
-      <p>This page is not yet Available....</p>
+      <p className="not-found">This page is not yet available...</p>
+        <img src={image} alt="Machine logo" width="400rem"/>
     </div>
   );
 }
