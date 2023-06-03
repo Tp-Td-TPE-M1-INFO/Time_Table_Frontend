dced98010cab1d963847d9ece0f3a942b2edb433
EssiJunior <nessipjunior@example.com>
2023-06-03 18:37:53 +0100

    feat(app): Some updates

--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -9,6 +9,7 @@ function NotFound() {
       <h1 className="title">OUPS!!!!!!!</h1>
       <p className="not-found">This page is not yet available...</p>
         <img src={image} alt="Machine logo" width="400rem"/>
+      <h3 style={{color:"#5a76b7"}}>404 - NOT FOUND</h3>
     </div>
   );
 }
