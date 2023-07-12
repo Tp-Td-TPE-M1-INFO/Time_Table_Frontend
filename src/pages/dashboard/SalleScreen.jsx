ddfd41a30ef392d4265494960b33bfdc9e450ae4
jocelyn pyw <jocelynwotcheu2@gmail.com>
2023-07-12 03:26:17 +0100

    resolution de certaines erreur 1/2

--- src/pages/dashboard/SalleScreen.jsx
+++ src/pages/dashboard/SalleScreen.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from 'react-redux'
-import ContainerC from '../../components/Container';
-import HallCreate from './components/CreateHallForm';
-import { getAllHall } from '../../redux/slices/hallSlice'
-import {useDeleteHallMutation, useUpdateHallMutation} from '../../redux/api'
+import { useSelector, useDispatch } from "react-redux";
+import ContainerC from "../../components/Container";
+import HallCreate from "./components/CreateHallForm";
+import { getAllHall } from "../../redux/slices/hallSlice";
+import { useDeleteHallMutation, useUpdateHallMutation } from "../../redux/api";
 import "../../index.css";
 import Box from "@mui/material/Box";
 import Sidebarr from "./sidebar";
@@ -18,39 +18,42 @@ import _ from "lodash";
 import Skeleton from "react-loading-skeleton";
 
 const SalleScreen = () => {
-
-  const data = useSelector((state) => state.hall.value)
-  const dispatch = useDispatch()
+  const data = useSelector((state) => state.hall.value);
+  const dispatch = useDispatch();
   const [deleteHall] = useDeleteHallMutation();
   const [updateHall] = useUpdateHallMutation();
 
   // State when processing
-  const [isEditing, setIsEditing] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  const [isEditing, setIsEditing] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     axios.get("/hall/allHalls").then(({ data }) => {
-        dispatch(getAllHall(data))
-        setIsLoading(false)
+      dispatch(getAllHall(data));
+      setIsLoading(false);
     });
-  }, [dispatch])    
+  }, [dispatch]);
 
   const del = (d) => {
-      console.log(d)
-      deleteHall(d._id).then(() => {
-          axios.get("/hall/allHalls").then(({ data }) => dispatch(getAllHall(data)));
-      });
-  }
+    // console.log(d)
+    deleteHall(d._id).then(() => {
+      axios
+        .get("/hall/allHalls")
+        .then(({ data }) => dispatch(getAllHall(data)));
+    });
+  };
   const update = (data) => {
-      updateHall(data).then(() => {
-          axios.get("/hall/allHalls").then(({ data }) => dispatch(getAllHall(data)));
-      });
-  }
+    updateHall(data).then(() => {
+      axios
+        .get("/hall/allHalls")
+        .then(({ data }) => dispatch(getAllHall(data)));
+    });
+  };
 
-  const editing = () =>{
-      setIsEditing(true)
-  }
+  const editing = () => {
+    setIsEditing(true);
+  };
   return (
     <div className="content">
       <Sidebarr />
@@ -88,29 +91,41 @@ const SalleScreen = () => {
                 <Box>
                   <IconButton>
                     <div className="addIconBox">
-                      <ContainerC component={ 
-                                  <AddIcon sx={{ color: "#fff" }} />
-                      } formToDisplay={<HallCreate/>} heading="Create hall"/>
+                      <ContainerC
+                        component={<AddIcon sx={{ color: "#fff" }} />}
+                        formToDisplay={<HallCreate />}
+                        heading="Create hall"
+                      />
                     </div>
                   </IconButton>
                 </Box>
               </Box>
             </Grid>
-            {   
-              !data ?
-              (_.range(8).map((index) => (
-                  <Grid key={index} md={3} xs={12} style={{margin:"1rem 0 0 2rem",}}>
-                      <Skeleton width={"15rem"} height={"8rem"}/>
+            {!data
+              ? _.range(8).map((index) => (
+                  <Grid
+                    key={index}
+                    md={3}
+                    xs={12}
+                    style={{ margin: "1rem 0 0 2rem" }}
+                  >
+                    <Skeleton width={"15rem"} height={"8rem"} />
                   </Grid>
-                ))):
-              data.map((d, key) => {
-                  return(
-                      <Grid item md={3} xs={12} key={key}>
-                          <BoxCard title={d.name} subTitle={d.capacity} deleteAction={() => del(d)} updateAction={() => update(d)} isEditing={isEditing} editingToggle={() => editing()}/>
-                      </Grid>
-                  )
-              })
-          }
+                ))
+              : data.map((d, key) => {
+                  return (
+                    <Grid item md={3} xs={12} key={key}>
+                      <BoxCard
+                        title={d.name}
+                        subTitle={d.capacity}
+                        deleteAction={() => del(d)}
+                        updateAction={() => update(d)}
+                        isEditing={isEditing}
+                        editingToggle={() => editing()}
+                      />
+                    </Grid>
+                  );
+                })}
           </Grid>
           {/* <p>Je suis dans la composante salle</p> */}
         </main>
